fe13e5d8d5248900ff27cb59c1347c529992bf21
compucter <egor-ruban@mail.ru>
2020-12-26 10:57:35 +0700

    finished story (but without pictures updating)

--- app/src/main/java/com/example/novella/EndActivity.kt
+++ app/src/main/java/com/example/novella/EndActivity.kt
@@ -1,22 +1,22 @@
 package com.example.novella
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.novella.databinding.StartScreenBinding
 
 private lateinit var binding : StartScreenBinding
-class MainActivity : AppCompatActivity() {
+class EndActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = StartScreenBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        binding.tvTitle.text = getString(R.string.start_title)
-        binding.tvBtnStart.text = getString(R.string.start_continue)
+
+        binding.tvTitle.text = getString(R.string.end_title)
+        binding.tvBtnStart.text = getString(R.string.end_continue)
         binding.tvBtnStart.setOnClickListener {
-            val intent = Intent(this, IntroActivity::class.java)
-            startActivity(intent)
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/novella/StoryActivity.kt
+++ app/src/main/java/com/example/novella/StoryActivity.kt
@@ -2,11 +2,11 @@ package com.example.novella
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.novella.databinding.ActivityStoryBinding
 import org.json.JSONArray
+import org.json.JSONObject
 
 private lateinit var binding : ActivityStoryBinding
 class StoryActivity : AppCompatActivity() {
@@ -15,40 +15,50 @@ class StoryActivity : AppCompatActivity() {
         binding = ActivityStoryBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
         val username = intent.getStringExtra("username") ?: "missed"
-        //
-        //reading whole scenario from from json file
-        //
+        val currentId = intent.getIntExtra("id", -1)
+        val currentStepJSON = getCurrentStepInfo(currentId)
+        updateLayout(currentStepJSON, username)
+    }
+
+    private fun getCurrentStepInfo(currentID : Int) : JSONObject{
         val jsonStr = resources.openRawResource(R.raw.scenario)
             .bufferedReader().use { it.readText() }
         val jsonArr = JSONArray(jsonStr)
-        val currentId = intent.getIntExtra("id", -1)
         var jsonObject = jsonArr.getJSONObject(0)
         for(i in 0 until jsonArr.length()){
             jsonObject = jsonArr.getJSONObject(i)
-            if(jsonObject.getInt("id") == currentId){
+            if(jsonObject.getInt("id") == currentID){
                 break
             }
         }
-        binding.tvKir.text = jsonObject.getString("title").replace("{username}",username)
-        val jsonAnswers = jsonObject.getJSONArray("answers")
+        return jsonObject
+    }
+
+    private fun updateLayout(currentStepJSON : JSONObject, username : String){
+        binding.tvKir.text = currentStepJSON.getString("title")
+            .replace("{username}", username)
+        val jsonAnswers = currentStepJSON.getJSONArray("answers")
         val howMuchAnswers = jsonAnswers.length()
         val choices = arrayOf(binding.secondOption, binding.thirdOption, binding.firstOption)
         for(i in choices.indices){
             if(howMuchAnswers > i){
-                with(choices[i]) {
+                with(choices[i]) {//todo добавить обновление картинок
                     val jsonAnswer = jsonAnswers.getJSONObject(i)
                     visibility = View.VISIBLE
                     text = jsonAnswer.getString("text")
                     setOnClickListener {
-                        val intent = Intent(baseContext, StoryActivity::class.java)
-                        intent.putExtra("id", jsonAnswer.getInt("nextID"))
+                        var intent = Intent(baseContext, EndActivity::class.java)
+                        if(jsonAnswer.getInt("nextID") != 14){
+                            intent = Intent(baseContext, StoryActivity::class.java)
+                            intent.putExtra("id", jsonAnswer.getInt("nextID"))
+                        }
                         startActivity(intent)
                         finish()
                     }
                 }
             }
         }
-        Log.d("MY_TAG", "${jsonAnswers.length()}")
     }
 }
